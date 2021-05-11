87ba80ad902d22f03074fb1bf1237c90488dfca5
asgdomhail@gmail.com <63181283+Asget-dev@users.noreply.github.com>
2021-05-10 17:01:17 -0700

    test login

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-class User{
-    constractor(fname,lname,phone,role,username,password,id,department){
-        this.fname = fname;
-        this.lname = lname;
-        this.phone = phone;
-        this.role = role;
-        this.username = username;
-        this.password = password;
-        this.id = id;
-        this.department = department;
-    }
+class User {
+  constructor(fname, lname, phone, role, username, password, id, department) {
+    this.fname = fname;
+    this.lname = lname;
+    this.phone = phone;
+    this.role = role;
+    this.username = username;
+    this.password = password;
+    this.id = id;
+    // this.department = department;
+  }
 }
-class Student{
-    constractor(fname, lname, phone, role, username, password, id, entryYear, gpa){
-        super(fname, lname, phone, role, username, password, id);
-        this.entryYear = entryYear;
-        this.gpa=gpa;
-    }
+class Student extends User {
+  constructor(fname, lname, phone, role, username, password, id) {
+    super(fname, lname, phone, role, username, password, id);
+  }
 }
 
-class Admin{
-    constractor(fname, lname, phone, role, username, password, id){
-        super(fname, lname, phone, role, username, password, id)
-    }
-}
-class Course{
-    constractor(Id, name, description, code){
-        super(Id, name, code);
-        this.description =description;
-    }
-}
-
+let stud1 = new Student("abc","def","206-818-1811","student","student", "1", "1");
+let stud2 = new Student("abc","def","206-818-1811","student","student", 1, 1);
+let allstud=[stud1,stud2];
 
 
 function login(){
-    let user = document.getElementById("user").value;
-    let password = document.getElementById("password").value;
+  let userID = document.getElementById("user").value;
+  let passWord = document.getElementById("password").value;
 
-    for(let i=){
-
+  for (let i = 0; i < allstud.length; i++){
+    if (allstud[i].id === userID && allstud[i].password === passWord) {
+      window.location=("student.html");
+      return;
     }
+}
+}
+window.onload=()=>{
+    document.getElementById("submit").onclick = login;
 }
